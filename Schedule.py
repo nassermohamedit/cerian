cfd2c832eb99977859a8f9fda3c8f0ebf2e72c08
Nasser Mohamed <nassermohamedit@gmail.com>
2024-02-27 15:48:14 +0100

    changed time-flow abstraction

--- Schedule.py
+++ Schedule.py
@@ -1,24 +1,38 @@
+import time
 from abc import ABC
 from datetime import timedelta, datetime
+import multiprocessing
 
 
 class Schedule(ABC):
-    def get_next_time(self):
+
+    def tik(self) -> bool:
         raise NotImplementedError()
 
 
 class Periodic(Schedule):
-    def __init__(self, minutes: int = 0, hours: int = 0, days: int = 0, start: datetime = None):
-        self.start = datetime.now().replace(second=0, microsecond=0) if start is None else start.replace(second=0, microsecond=0)
+    def __init__(self, max_delay, seconds: int = 0, minutes: int = 0, hours: int = 0, days: int = 0,
+                 start: datetime = None):
+        self.start = datetime.now() if start is None else start
         self.period = None
-        if minutes != 0 or hours != 0 or days != 0:
-            self.period = timedelta(minutes=minutes, hours=hours, days=days)
+        if seconds != 0 or minutes != 0 or hours != 0 or days != 0:
+            self.period = timedelta(seconds=seconds, minutes=minutes, hours=hours, days=days)
+        self.last_time = None
+        self.max_delay = max_delay
 
-    def get_next_time(self):
-        generations = self.no_generations()
-        return self.start + self.period * generations
-
-    def no_generations(self):
-        now = datetime.now().replace(microsecond=0, second=0)
-        return (now - self.start) // self.period + 1
-
+    def tik(self) -> bool:
+        now = datetime.now()
+        if now < self.start:
+            return False
+        if self.last_time is None:
+            if now < self.start + self.max_delay:
+                self.last_time = self.start
+                return True
+            self.last_time = self.start + ((now - self.start) // self.period) * self.period
+            return self.tik()
+        if self.period <= now - self.last_time <= self.period + self.max_delay:
+            self.last_time += self.period
+            return True
+        if self.last_time < now:
+            self.last_time += self.period * ((now - self.last_time) // self.period)
+        return False
