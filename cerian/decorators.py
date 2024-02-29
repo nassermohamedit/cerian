5eddd70483c5652e457b88c70125d77c2c91ab90
Nasser Mohamed <nassermohamedit@gmail.com>
2024-02-29 19:16:54 +0100

    added docstrings

--- cerian/decorators.py
+++ cerian/decorators.py
@@ -4,6 +4,7 @@ from schedule import Periodic
 
 
 def job(func):
+    """Decorate a function that represent job."""
     @functools.wraps(func)
     def wrapper():
         func()
@@ -12,6 +13,7 @@ def job(func):
 
 
 def periodic(period: str | timedelta, start: datetime = None, max_delay: str | timedelta = None):
+    """Decorate job functions that must run in a periodic manner."""
     def periodic_(func):
         def wrapper():
             func()
