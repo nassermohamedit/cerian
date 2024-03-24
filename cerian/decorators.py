902838188a3828c796899a40ac415e38f31abde0
Nasser Mohamed <nassermohamedit@gmail.com>
2024-03-24 11:40:28 +0000

    fixed import method

--- cerian/decorators.py
+++ cerian/decorators.py
@@ -1,6 +1,7 @@
 import functools
 from datetime import datetime, timedelta
-from schedule import Periodic
+from typing import Optional
+from cerian.schedule import Periodic
 
 
 def job(func):
@@ -12,7 +13,7 @@ def job(func):
     return wrapper
 
 
-def periodic(period: str | timedelta, start: datetime = None, err: str | timedelta = None):
+def periodic(period: str | timedelta, start: Optional[datetime] = None, err: Optional[str | timedelta] = None):
     """Decorate job functions that must run in a periodic schedule."""
     def periodic_(func):
         def wrapper():
